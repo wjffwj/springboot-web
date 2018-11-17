7e551595cf07d2e7e381b7b0b83fd15645fdcfd2
魏俊峰 <2474530605@qq.com>
2018-11-17 22:08:21 +0800

    springBoot 提供的mvc扩展功能 继承WebMvcConfigurerAdapter  ,且使用@Configuration声明此类是配置类

---
+++ src/main/java/com/wjf/config/MVCConfig.java
@@ -0,0 +1,16 @@
+package com.wjf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//使用webMVCconfigureAdapter可以扩展MVC的功能
+@Configuration
+//@EnableWebMvc
+public class MVCConfig  extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //super.addViewControllers(registry);
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+}
